7cdd038943da6b7442cc713da291b06da7615d47
komed3 <webmaster@komed3.de>
2026-06-25 13:02:46 +0200

    create cron parser cls

---
+++ src/CronParser.ts
@@ -0,0 +1,14 @@
+/**
+ * CronParser
+ * Parses and validates cron expressions.
+ */
+
+/**
+ * CronParser parses and validates cron expressions into structured objects.
+ * 
+ * @example
+ * const parser = new CronParser();
+ * const parsed = parser.parse( '0 9 * * MON' );
+ * const valid = parser.validate( '0 9 * * MON' );
+ */
+export class CronParser {}
