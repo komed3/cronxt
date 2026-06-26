0e45b113b4f70f4610ca76399f70f97f54c53595
komed3 <webmaster@komed3.de>
2026-06-26 11:06:54 +0200

    Update parser.ts

--- src/parser.ts
+++ src/parser.ts
@@ -9,14 +9,7 @@ import type {
   ParsedFieldComponent, SpecialAlias
 } from './types';
 
-/**
- * Parses and validates cron expressions into structured objects.
- * 
- * @example
- * const parser = new CronParser();
- * const parsed = parser.parse( '0 9 * * MON' );
- * const valid = parser.validate( '0 9 * * MON' );
- */
+/** Parses and validates cron expressions into structured objects. */
 export class CronParser {
   private static readonly SPLIT = /\s+/;
   private static instance?: CronParser;
