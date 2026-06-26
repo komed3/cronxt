0cff4a6f6536a9d7c68a0cecd4ca65248d3e935f
komed3 <webmaster@komed3.de>
2026-06-26 23:34:37 +0200

    Create builder.ts

---
+++ src/builder.ts
@@ -0,0 +1,19 @@
+import { CRON_DEFAULTS } from './const';
+import { CronParser } from './parser';
+import type { CronFieldName, CronObject, SpecialAlias } from './types';
+
+/** Immutable fluent builder for cron expressions. */
+export class CronBuilder {
+  private static parser = CronParser.getInstance();
+
+  private readonly fields: CronObject;
+  private readonly current?: CronFieldName;
+
+  public static create ( expr?: string | SpecialAlias | CronObject ) : CronBuilder {
+    return new CronBuilder( expr ? typeof expr === 'string' ? CronBuilder.parser.toObject( expr ) : expr : CRON_DEFAULTS );
+  }
+
+  private constructor ( fields: CronObject, current?: CronFieldName ) {
+    this.fields = { ...fields }, this.current = current;
+  }
+}
