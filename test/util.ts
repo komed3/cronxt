8e70e32838a83fa0ed20c09e0f43b8201babdcdd
komed3 <webmaster@komed3.de>
2026-06-27 13:14:42 +0200

    register and execute a test case

--- test/util.ts
+++ test/util.ts
@@ -1,4 +1,22 @@
-// Test Utility
+/**
+ * Test Utility
+ * Minimal test utility.
+ */
 
 let passed = 0;
 let failed = 0;
+
+/** Register and execute a test case. */
+export function test ( name: string, fn: () => void ) : void {
+  try {
+    fn();
+    passed++;
+
+    console.log( `✓ ${ name }` );
+  } catch ( err ) {
+    failed++;
+
+    console.error( `✗ ${ name }` );
+    console.error( err );
+  }
+}
